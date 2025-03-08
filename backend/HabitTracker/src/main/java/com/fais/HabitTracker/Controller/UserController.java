e88b13ec07e971fce5c46e382793563c2c5a50f8
OpalinskiJakub <jakopal1125@gmail.com>
2025-03-08 17:50:33 +0100

    #10 - basic spring security configuration was added

---
+++ backend/HabitTracker/src/main/java/com/fais/HabitTracker/Controller/UserController.java
@@ -0,0 +1,15 @@
+package com.fais.HabitTracker.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "user";
+    }
+}
