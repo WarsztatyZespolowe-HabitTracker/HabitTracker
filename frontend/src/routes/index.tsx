e4dd842cd8b44d42a171b1defd4c70ce9241090a
LukaszzM <lukaszzmalucha@gmail.com>
2025-03-22 13:57:40 +0100

    add landing page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,16 @@
+import { Header } from "@/components/home/header";
+import { Hero } from "@/components/home/hero";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-	component: App,
+	component: HomePage,
 });
 
-function App() {
+function HomePage() {
 	return (
-		<div className="text-center">
-			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white">
-				<h1>Habit Tracker</h1>
-			</header>
+		<div className="flex min-h-screen flex-col">
+			<Header />
+			<Hero />
 		</div>
 	);
 }
